ccc7252fabfdb5c1941177a5bee5367a88ab6048
Pedro Paixao <paixaop@gmail.com>
2015-09-06 21:03:20 -0400

    comment code

--- bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/ZWaveSceneController.java
+++ bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/ZWaveSceneController.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveCommandClass;
+import org.openhab.binding.zwave.internal.protocol.ZWaveDeviceClass.Specific;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
@@ -35,36 +36,118 @@ public class ZWaveSceneController {
 	private ZWaveNode node;
 	private int numberOfGroups;
 	private boolean isCooperController;
+	private boolean isPortable;
+	private ZWaveController controller;
 
-	ZWaveSceneController() {
-		node = null;
+	/**
+	 * Constructor with just a Z-Wave controller object
+	 * @param ctrl Z-Wave Controller object
+	 */
+	ZWaveSceneController(ZWaveController ctrl) {
+		init(ctrl, null);
+	}
+
+	/**
+	 * Constructor
+	 * @param ctrl Z-Wave Controller object
+	 * @param newNode ZWaveNode of the scene controller device
+	 */
+	ZWaveSceneController(ZWaveController ctrl, ZWaveNode newNode) {
+		init(ctrl, newNode);
+	}
+	
+	/**
+	 * Constructor
+	 * @param ctrl Z-Wave Controller object
+	 * @param nodeId int node ID of scene controller device
+	 */
+	ZWaveSceneController(ZWaveController ctrl, int nodeId) {
+		init(ctrl, ctrl.getNode(nodeId));
+	}
+	
+	/**
+	 * Initialize object
+	 * @param ctrl Z-Wave Controller object
+	 * @param newNode ZWaveNode object
+	 */
+	private void init(ZWaveController ctrl, ZWaveNode newNode) {
+		controller = ctrl;
 		numberOfGroups = 0;
 		isCooperController = false;
-	}
-
-	ZWaveSceneController(ZWaveNode newNode) {
+		isPortable = false;
 		setNode(newNode);
 	}
-
+	
+	/**
+	 * Set a Z-Wave node as a Scene Controller
+	 * @param n ZWaveNode 
+	 */
 	public void setNode(ZWaveNode n) {
-		logger.debug("Set Node {}", n.getNodeId());
-		node = n;
-
-		// Check if node supports scene activation
-		ZWaveCommandClass c = node.getCommandClass(ZWaveCommandClass.CommandClass.SCENE_CONTROLLER_CONF);
-		if (c != null) {
-			logger.error("NODE {} : This device does not support scene controller configuration command class and cannot be configured for scenes.", node.getNodeId());
+		
+		if (n==null) {
+			logger.debug("Node cannot be null. Ignoring.", n.getNodeId());
 			return;
 		}
 		
-		if ( node.getManufacturer() == COOPER
+		// Check if this node is a Scene Controller
+		ZWaveDeviceClass deviceClass = n.getDeviceClass();
+		if (deviceClass.getSpecificDeviceClass() == ZWaveDeviceClass.Specific.PORTABLE_SCENE_CONTROLLER ||
+			deviceClass.getSpecificDeviceClass() == ZWaveDeviceClass.Specific.SCENE_CONTROLLER) {
+			
+			if (deviceClass.getSpecificDeviceClass() == ZWaveDeviceClass.Specific.PORTABLE_SCENE_CONTROLLER) {
+				isPortable = true;
+			}
+			
+			if(n.getManufacturer() == COOPER) {
+				isCooperController = true;
+			}
+			
+		}
+		else {
+			logger.debug("Node {} is not a Scene Controller. Ignoring.", n.getNodeId());
+			return;
+		}
+		
+		// Check if node supports scene activation
+		ZWaveCommandClass c = n.getCommandClass(ZWaveCommandClass.CommandClass.SCENE_CONTROLLER_CONF);
+		if (c != null) {
+			logger.error("NODE {} : This device does not support scene controller configuration command class and cannot be configured for scenes.", n.getNodeId());
+			return;
+		}
+		
+		logger.debug("Scene Controller Set Node {}", n.getNodeId());
+		node = n;
+	}
+	
+	/**
+	 * Set a Z-Wave node as a scene controller using node ID
+	 * @param nodeId int ID of the node
+	 */
+	public void setNode(int nodeId) {
+		setNode(controller.getNode(nodeId));
 	}
 
+	/**
+	 * Get the scene controller node 
+	 * @return node
+	 */
 	public ZWaveNode getNode() {
 		return node;
 	}
 
-	public boolean isCooper() {
+	/**
+	 * Is the device a Copper Scene Controller
+	 * @return boolean true if device is a cooper scene controller
+	 */
+	public boolean isCooperController() {
 		return isCooperController;
+	}
+	
+	/**
+	 * Is the device a portable scene controller or static
+	 * @return boolean true if device is a protable scene controller
+	 */
+	public boolean isPortableController() {
+		return isPortable;
 	}
 }
